b8f568f1986305cf7fed8d25f62de6c491c823ec
Jessiah Ratliff <jessiahr@gmail.com>
2017-08-29 22:23:23 -0700

    add docs

--- mix.exs
+++ mix.exs
@@ -53,7 +53,8 @@ defmodule TpLinkHs100.Mixfile do
   # Type "mix help deps" for more examples and options
   defp deps do
     [
-      {:poison, "~> 3.0"}
+      {:poison, "~> 3.0"},
+      {:ex_doc, ">= 0.0.0", only: :dev}
     ]
   end
 end
